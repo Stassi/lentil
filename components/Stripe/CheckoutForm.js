b61538911e1b3affe763a38cdad298c94a10167c
Andreas Stassivik <andreas@stassi.net>
2019-11-05 04:31:50 -0800

    CheckoutForm

---
+++ components/Stripe/CheckoutForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { CardElement } from 'react-stripe-elements'
+
+class CheckoutForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { complete: false }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  async handleSubmit (ev) {
+    const { token } = await this.props.stripe.createToken({ name: 'Name' })
+    const response = await fetch('/charge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
+      body: token.id
+    })
+
+    if (response.ok) this.setState({ complete: true })
+  }
+
+  render () {
+    if (this.state.complete) return <h1>Purchase Complete</h1>
+
+    return (
+      <div className='checkout'>
+        <p>Would you like to complete the purchase?</p>
+        <CardElement />
+        <button onClick={this.handleSubmit}>Purchase</button>
+      </div>
+    )
+  }
+}
+
+export default CheckoutForm
