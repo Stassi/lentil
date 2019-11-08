6e5b081bcefd495a9727de945120da6b9ab781db
Andreas Stassivik <andreas@stassi.net>
2019-11-07 19:33:30 -0800

    MUI table replaces raw data

--- components/StripeCardDebugger.js
+++ components/StripeCardDebugger.js
@@ -1,6 +1,26 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 import normalizeParameters from '../src/enhancers/normalizeStripeCard'
 import stringifyBoolean from '../src/utility/stringifyBoolean'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+    width: '100%'
+  },
+  table: {
+    minWidth: 220
+  }
+}))
+
+const createData = (field, value) => ({ field, value })
 
 const StripeCardDebugger = ({
   brand,
@@ -11,28 +31,53 @@ const StripeCardDebugger = ({
   inputEmpty: inputEmptyBool,
   postalCode
 }) => {
+  const {
+    root: rootClass,
+    table: tableClass
+  } = useStyles()
+
   const [inputComplete, inputEmpty] = [
     inputCompleteBool,
     inputEmptyBool
   ].map(stringifyBoolean)
 
+  const rows = [
+    createData('Brand', brand),
+    createData('Postal code', postalCode),
+    createData('Input complete', inputComplete),
+    createData('Input empty', inputEmpty),
+    createData('Error code', errorCode),
+    createData('Error message', errorMessage),
+    createData('Error type', errorType)
+  ]
+
   return (
-    <>
-      <br />
-      Brand: {brand}
-      <br />
-      Postal code: {postalCode}
-      <br />
-      Empty: {inputEmpty}
-      <br />
-      Complete: {inputComplete}
-      <br />
-      Error code: {errorCode}
-      <br />
-      Error message: {errorMessage}
-      <br />
-      Error type: {errorType}
-    </>
+    <Paper className={rootClass}>
+      <Table className={tableClass}>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              Field
+            </TableCell>
+            <TableCell align='right'>
+              Value
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(({ field, value }) => (
+            <TableRow key={field}>
+              <TableCell component='th' scope='row'>
+                {field}
+              </TableCell>
+              <TableCell align='right'>
+                {value}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   )
 }
 
