366c822238c09ec4a585bab3e6b201873b804af6
Andreas Stassivik <andreas@stassi.net>
2019-11-08 04:25:59 -0800

    useElements HOC

---
+++ components/Stripe/useElements.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Elements,
+  injectStripe,
+  StripeProvider
+} from 'react-stripe-elements'
+import getDisplayName from '../../src/utility/getDisplayName'
+
+const enhance = apiKey => Component => ({ ...props }) => {
+  const [stripe, setStripe] = useState(null)
+
+  useEffect(() => { setStripe(window.Stripe(apiKey)) }, [])
+
+  return (
+    <StripeProvider {...{ stripe }}>
+      <Elements>
+        <Component {...props} />
+      </Elements>
+    </StripeProvider>
+  )
+}
+
+const useElements = apiKey => Component => {
+  const StripeInjectedComponent = injectStripe(Component)
+  const [EnhancedComponent, targetName] = [
+    enhance(apiKey)(StripeInjectedComponent),
+    getDisplayName(StripeInjectedComponent)
+  ]
+  EnhancedComponent.displayName = `WithStripeElements(${targetName})`
+  return EnhancedComponent
+}
+
+export default useElements
