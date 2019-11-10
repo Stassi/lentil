7aef245dbaa78d146faddedf232cbe23153c53e0
Andreas Stassivik <andreas@stassi.net>
2019-11-10 04:15:21 -0800

    extract logo dimensions json

--- src/brandLogo/brandSafeTopPadding.js
+++ src/brandLogo/brandSafeTopPadding.js
@@ -1,39 +1,14 @@
 import heightToWidthPercentage from '../utility/heightToWidthPercentage'
+import logoDimensions from './logoDimensions'
+
+const {
+  stripe: stripeDimensions,
+  ...cardLogos
+} = logoDimensions
 
 const brandSafeTopPadding = {
-  paddingTop: ({ brand }) => heightToWidthPercentage(({
-    amex: {
-      height: 997.51703,
-      width: 1000
-    },
-    diners: {
-      height: 504,
-      width: 1945
-    },
-    discover: {
-      height: 21.356791,
-      width: 126.85181
-    },
-    jcb: {
-      height: 231.511,
-      width: 300
-    },
-    mastercard: {
-      height: 120.41,
-      width: 146.8
-    },
-    unionpay: {
-      height: 160,
-      width: 256
-    },
-    visa: {
-      height: 323.653,
-      width: 1000.046
-    }
-  })[brand] || {
-    height: 222.5,
-    width: 468
-  })
+  paddingTop: ({ brand }) =>
+    heightToWidthPercentage((cardLogos)[brand] || stripeDimensions)
 }
 
 export default brandSafeTopPadding
