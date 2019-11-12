62b89caa0f2727c9343900d33c514ae9166a1629
Andreas Stassivik <andreas@stassi.net>
2019-11-11 22:24:55 -0800

    simplify logo index

--- src/brandLogo/brandLogo.js
+++ src/brandLogo/brandLogo.js
@@ -1,13 +1,11 @@
-import {
-  amex,
-  diners,
-  discover,
-  jcb,
-  mastercard,
-  stripe,
-  unionpay,
-  visa
-} from './logos'
+import amex from './logos/amex.svg'
+import diners from './logos/diners.svg'
+import discover from './logos/discover.svg'
+import jcb from './logos/jcb.svg'
+import mastercard from './logos/mastercard.svg'
+import stripe from './logos/stripe.svg'
+import unionpay from './logos/unionpay.svg'
+import visa from './logos/visa.svg'
 
 const brandLogo = brand => ({
   amex,
