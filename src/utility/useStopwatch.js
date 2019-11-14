16eee465ee9917138ef8fe52eb25e7a928946a4f
Andreas Stassivik <andreas@stassi.net>
2019-11-13 18:51:27 -0800

    stopwatch hook

---
+++ src/utility/useStopwatch.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+const useStopwatch = ({ interval, on }) => {
+  const [time, setTime] = useState(null)
+
+  useEffect(() => { if (on) { setTime(0) } }, [on])
+
+  useEffect(() => {
+    if (typeof time === 'number') {
+      const id = setTimeout(() => { setTime(time + interval) }, interval)
+      return () => { clearTimeout(id) }
+    }
+  }, [time])
+
+  return time
+}
+
+export default useStopwatch
