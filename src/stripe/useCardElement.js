5702f00b2dbf47f2ca3e02eb6ea8293d8a590f16
Andreas Stassivik <andreas@stassi.net>
2019-11-15 15:23:49 -0800

    useCardElement hook

---
+++ src/stripe/useCardElement.js
@@ -0,0 +1,33 @@
+import React, {
+  useEffect,
+  useState
+} from 'react'
+import { CardElement as StripeCardElement } from 'react-stripe-elements'
+
+const useCardElement = ({ focus }) => {
+  const [Component, setComponent] = useState(null)
+  const [element, setElement] = useState(null)
+  const [stripeCard, setStripeCard] = useState({})
+
+  useEffect(() => {
+    setComponent(
+      <StripeCardElement
+        onChange={setStripeCard}
+        onReady={setElement}
+      />
+    )
+  }, [])
+
+  useEffect(() => {
+    if (focus && element) element.focus()
+  }, [element])
+
+  return {
+    Component,
+    element,
+    stripeCard,
+    loaded: !!element
+  }
+}
+
+export default useCardElement
