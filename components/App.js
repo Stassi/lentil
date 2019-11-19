3c0db3d655338ab1202d52a0b10831d805c148f5
Andreas Stassivik <andreas@stassi.net>
2019-11-19 00:25:38 -0800

    implement useApp

---
+++ components/App.js
@@ -0,0 +1,5 @@
+import useApp from '../src/useApp'
+
+const App = pageProps => useApp(pageProps)
+
+export default App
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import App from 'next/app'
-import Head from 'next/head'
-import { ThemeProvider } from '@material-ui/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import theme from '../src/theme'
+import NextApp from 'next/app'
+import App from '../components/App'
 
-export default class MyApp extends App {
+export default class MyApp extends NextApp {
   componentDidMount () {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
@@ -14,20 +11,5 @@ export default class MyApp extends App {
     }
   }
 
-  render () {
-    const { Component, pageProps } = this.props
-
-    return (
-      <>
-        <Head>
-          <title>My page</title>
-        </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </>
-    )
-  }
+  render = () => <App {...this.props} />
 }
