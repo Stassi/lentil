7e2065cfb49b22344a74b2a69792b29b31a3a3dc
Andreas Stassivik <andreas@stassi.net>
2019-11-19 17:23:36 -0800

    remove document theme

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ import Document, {
   NextScript
 } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/styles'
-import theme from '../src/theme'
 
 class MyDocument extends Document {
   render () {
@@ -17,8 +16,6 @@ class MyDocument extends Document {
             name='viewport'
             content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
           />
-          {/* PWA primary color */}
-          <meta name='theme-color' content={theme.palette.primary.main} />
           <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
