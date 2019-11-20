7d5151425756c4c0bed98af1e3622f549c668d41
Andreas Stassivik <andreas@stassi.net>
2019-11-19 17:24:14 -0800

    remove synchronous stripe client

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,6 @@ class MyDocument extends Document {
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
-          <script src='https://js.stripe.com/v3/'></script>
         </Head>
         <body>
           <Main />
