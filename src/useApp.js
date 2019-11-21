244052219183687828b7c9b8d14d788fa270ffc5
Andreas Stassivik <andreas@stassi.net>
2019-11-20 17:11:36 -0800

    implement useMuiTheme

--- src/useApp.js
+++ src/useApp.js
@@ -1,41 +1,22 @@
 import React, {
   useEffect,
-  useMemo,
   useReducer
 } from 'react'
 import Head from 'next/head'
 import { Elements, StripeProvider } from 'react-stripe-elements'
+import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import apiTestKeys from '../src/stripe/apiTestKeys'
+import useMuiTheme from './useMuiTheme'
 import useStripe from './useStripe'
 
 const { publishable: publishableKey } = apiTestKeys
 
 const useApp = ({ Component, ...pageProps }) => {
-  const initialState = {
-    nullifyStyles: true,
-    themeObject: {
-      palette: {
-        primary: {
-          main: '#6772e5'
-        },
-        type: 'dark'
-      }
-    },
-    titleText: 'lentil'
-  }
+  const initialState = { nullifyStyles: true, titleText: 'lentil' }
 
-  const [
-    {
-      nullifyStyles,
-      themeObject,
-      titleText
-    },
-    dispatch
-  ] = useReducer((prevState, action) => {
+  const [{ nullifyStyles, titleText }, dispatch] = useReducer((prevState, action) => {
     if (action.type === 'reset') return initialState
-    if (action.type === 'setTheme') return { ...prevState, themeObject: action.theme }
     if (action.type === 'setTitle') return { ...prevState, titleText: action.title }
     throw new Error()
   }, initialState)
@@ -44,10 +25,7 @@ const useApp = ({ Component, ...pageProps }) => {
     return () => dispatch({ type: 'reset' })
   }, [])
 
-  const theme = useMemo(
-    () => createMuiTheme(themeObject),
-    [themeObject]
-  )
+  const { theme, ...themeProps } = useMuiTheme()
 
   const { instance: stripe, ...stripeProps } = useStripe({ publishableKey })
 
@@ -66,23 +44,13 @@ const useApp = ({ Component, ...pageProps }) => {
           <Elements>
             <Component
               {...{
-                ...stripeProps,
                 stripe,
                 theme,
-                themeObject,
                 titleText,
-                setTheme: theme => dispatch({ theme, type: 'setTheme' }),
-                setTitle: title => dispatch({ title, type: 'setTitle' }),
-                toggleDarkOrLightTheme: () => dispatch({
-                  theme: {
-                    palette: {
-                      ...themeObject.palette,
-                      type: theme.palette.type === 'light' ? 'dark' : 'light'
-                    }
-                  },
-                  type: 'setTheme'
-                }),
-                ...pageProps
+                ...stripeProps,
+                ...themeProps,
+                ...pageProps,
+                setTitle: title => dispatch({ title, type: 'setTitle' })
               }}
             />
           </Elements>
